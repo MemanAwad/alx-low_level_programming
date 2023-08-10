40188262ff3a356f58ca2710d741668505dd4b2d
MemanAwad <meman1awad@gmail.come>
2023-08-10 15:31:45 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This function will print some text
+ *
+ * Return: this function will return 1 to end program
+ */
+int main(void)
+{
+		printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+		return (1);
+}
