51f7c85f1a293706370a945a16eed62d94b1abeb
MemanAwad <meman1awad@gmail.come>
2023-08-10 12:48:48 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This function will print some text
+ *
+ * Return: this function will return zero to end program
+ */
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle\n");
+		return (0);
+}
