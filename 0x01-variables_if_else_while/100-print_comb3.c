7f45a53a94d4f8f351f20a566d24f9728289d165
MemanAwad <meman1awad@gmail.come>
2023-08-11 15:45:03 -0700

    diff

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - this function will print the numbers from 0-9
+ * Return: this function will return 0 to end the program
+ */
+	int main(void)
+	{
+		int x;
+		int y;
+
+		for (x = 48; x < 58; x++)
+		{
+			for (y = x + 1 ; y < 58; y++)
+			{
+				if (x != y)
+					putchar(x);
+					putchar(y);
+
+				if (x < 57 && x != y)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
