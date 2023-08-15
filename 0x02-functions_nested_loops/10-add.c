a9b32bab21cf9301b1ad2b16f09e9b02aae498b3
MemanAwad <meman1awad@gmail.come>
2023-08-15 15:18:18 -0700

    add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - return the last digit of input number
+ * @digit: it is the input to give its absoulute value
+ * Return: integar value
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
