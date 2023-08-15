ffc86d1a5d7983640a2f6d213607d51b4ba5e3ce
MemanAwad <meman1awad@gmail.come>
2023-08-15 11:26:17 -0700

    10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - this function will print text _putchar
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+}
