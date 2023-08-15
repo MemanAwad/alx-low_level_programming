590d854a23080171b030dba5c1e0e4ad5e36d522
MemanAwad <meman1awad@gmail.come>
2023-08-15 15:02:13 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - return the last digit of input number
+ * @digit: it is the input to give its absoulute value
+ * Return: integar value
+ */
+
+int print_last_digit(int digit)
+{
+	int var;
+
+	var = abs(digit%10);
+	return (var);
+}
