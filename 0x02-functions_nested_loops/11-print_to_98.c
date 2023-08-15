78d19c05a3ade12456eacda6fc4bff722bbac834
MemanAwad <meman1awad@gmail.come>
2023-08-15 15:48:21 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,25 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+			}
 		}
 	}
 	else if (n == 98)
 	{
 		printf("%d", n);
-	}
 	else 
 	{
 		int i;
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+			}
 		}
 	}
 }
