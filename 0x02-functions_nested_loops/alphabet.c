3c24c74f058602bd1f4a4c6005e28402665bc13a
MemanAwad <meman1awad@gmail.come>
2023-08-15 10:51:22 -0700

    alpha

---
+++ 0x02-functions_nested_loops/alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this function will print text _putchar
+ * Return: Always zero
+ */
+int main(void)
+{
+	print-alphabet();
+	return (0);
+}
+void print_alphabet (void)
+{
+	char ch = 'a';
+
+	while(ch < 'z')
+	{
+		putchar(ch);
+	}
+}
