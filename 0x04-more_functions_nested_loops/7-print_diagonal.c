459faab97c7e82be991dec72ba7491cb0c2d45f6
MemanAwad <meman1awad@gmail.come>
2023-08-17 11:44:36 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 {
 	if (n == 0 || n < 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -22,10 +22,10 @@ void print_diagonal(int n)
 
 			for (j = 0; j < i; j++)
 			{
-				putchar(32);
+				_putchar(32);
 			}
-			putchar(92);
-			putchar('\n');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 }
