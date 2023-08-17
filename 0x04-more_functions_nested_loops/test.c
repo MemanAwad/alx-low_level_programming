98adfef206c1b99902bbc1a83a47c2302ef06385
MemanAwad <meman1awad@gmail.come>
2023-08-17 10:45:55 -0700

    more numbers

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * more_numbers - this function will print numbers from 0 to 14 ten times
- * Return: void
- */
+
+void more_numbers(void);
+
+int main(void)
+{
+	more_numbers();
+	return (0);
+}
 void more_numbers(void)
 {
 	int i;
