74d69958e6f5ed8a493112e2b274ddc09e2ea7b7
MemanAwad <meman1awad@gmail.come>
2023-08-17 13:11:15 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * fizzbuzz - this function will multiply 2 digit and return the result
+ * Return: void
+ *
+ */
+void fizzbuzz (void);
+
+int main(void)
+{
+	fizzbuzz();
+	printf("\n");
+}
+void fizzbuzz(void)
+{
+	char f[5] = "Fizz";
+	char b[5] = "Buzz";
+	char fb[10] = "FizzBuzz";
+	int i;
+	
+	printf("%d", 1);
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" %s", fb);
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" %s", f);
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" %s", b);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+}
