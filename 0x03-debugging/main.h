dc8caa469a349df824de4224356f73a3cdbdb301
MemanAwad <meman1awad@gmail.come>
2023-08-18 06:07:58 -0700

    fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,8 @@
-#include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+fndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
