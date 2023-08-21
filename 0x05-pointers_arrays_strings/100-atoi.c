12a0f6b8b4bf40f8865be93cc144d03dcf3bfadb
MemanAwad <meman1awad@gmail.come>
2023-08-21 14:46:06 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _atoi - will convert string to integer
+ * @s: string to be converted
+ * Return: integer value of the string
+ */
+int _atoi(char *s)
+{
+	int val ;
+
+	val = atoi(s);
+
+	return (val);
+}
