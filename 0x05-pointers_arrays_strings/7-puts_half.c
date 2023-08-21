d54358f1b502efc3a61d8ee663f5a346b39fd4d2
MemanAwad <meman1awad@gmail.come>
2023-08-21 13:19:27 -0700

    other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,14 @@ void puts_half(char *str)
 	int i;
 	int len = strlen(str);
 	int x;
-	
+
 	if (len % 2 == 0)
 	{
 		x = len / 2;
 	}
 	else
 	{
-		x = (len / 2 ) - 1;
+		x = (len / 2) - 1;
 	}
 	for (i = x; i < len; i++)
 	{
