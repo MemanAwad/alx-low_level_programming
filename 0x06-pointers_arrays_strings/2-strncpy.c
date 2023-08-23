8319458b6847059254acf141e3b498639bf5b714
MemanAwad <meman1awad@gmail.come>
2023-08-23 10:46:45 -0700

    strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * len - will return the length of a string
+ * @c: the string
+ * Return: integer value off the length
+ */
+int len(char *c)
+{
+	int len = 0;
+
+	while (c[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strncpy - This function will copy the string
+ * @dest : the copied string
+ * @src : the string will be copied
+ * @n : number of character to be copied
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int lensrc = len(src);
+
+	if (n > lensrc)
+	{
+		int i;
+
+		for (i = 0; i <  lensrc; i++)
+		{
+			dest[i] = src[i];
+		}
+		dest[i] = '\0';
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <  n; i++)
+		{
+			dest[i] = src[i];
+		}
+	}
+	return (dest);
+}
