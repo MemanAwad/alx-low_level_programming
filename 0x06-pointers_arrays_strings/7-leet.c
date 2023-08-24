1d9677caa486705265d8066d36bd7f1d2607d163
MemanAwad <meman1awad@gmail.come>
2023-08-24 11:07:18 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ *cap_string - capitalize every word in the string
+ * @ch : the string
+ * Return: camelcase string
+ */
+char *cap_string(char *ch)
+{
+	int i = 0;
+	char x;
+
+	while (ch[i] != '\0')
+	{
+		i++;
+		if (ch[i - 1] == ',' ||
+		ch[i - 1] == ';' ||
+		ch[i - 1] == ' ' ||
+		ch[i - 1] == '\n' ||
+		ch[i - 1] == '\t' ||
+		ch[i - 1] == '.' ||
+		ch[i - 1] == '!' ||
+		ch[i - 1] == '?' ||
+		ch[i - 1] == '\"' ||
+		ch[i - 1] == '(' ||
+		ch[i - 1] == ')' ||
+		ch[i - 1] == '{' ||
+		ch[i - 1] == '}' ||
+		i == 0)
+		{
+			if ((ch[i] >= 'A' && ch[i] <= 'Z') ||
+			!(ch[i] >= 'a' && ch[i] <= 'z'))
+			{
+				x = ch[i];
+			}
+			else
+			{
+				x = ch[i] - 32;
+			}
+			ch[i] = x;
+		}
+
+	}
+	return (ch);
+}
