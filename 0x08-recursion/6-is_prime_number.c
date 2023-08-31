5136b91a7b7360452738e42f424c2286a342e3b0
MemanAwad <meman1awad@gmail.come>
2023-08-31 13:16:57 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,7 +29,7 @@ int iterate(int num, int num2)
 
 int is_prime_number(int n)
 {
-	if (n == 1 || n == -1)
+	if (n == 1 || n < 0)
 	{
 		return (0);
 	}
@@ -39,10 +39,6 @@ int is_prime_number(int n)
 	}
 	else
 	{
-		if (n < 0)
-		{
-			n = n * -1;
-		}
 		return (iterate(n, n - 1));
 	}
 }
