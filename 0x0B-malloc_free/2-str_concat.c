c1518e7923e4b26b5c799d25bb377a6cd5d446f2
MemanAwad <138756696+MemanAwad@users.noreply.github.com>
2023-09-05 13:50:21 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,10 +33,9 @@ char *str_concat(char *s1, char *s2)
 	{
 		s[i] = s1[i];
 	}
-	s[i] = ' ';
-	for (i = 1; i <= len2; i++)
+	for (i = 0; i < len2; i++)
 	{
-		s[i + len] = s2[i - 1];
+		s[i + len] = s2[i];
 	}
 	s[i + len] = '\0';
 	return (s);
