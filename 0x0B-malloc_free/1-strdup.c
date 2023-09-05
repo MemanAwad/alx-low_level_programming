a9cebbb6c6c7a6d249e31985dca0c64831421e87
MemanAwad <meman1awad@gmail.come>
2023-09-05 03:31:32 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,11 +11,11 @@ char *_strdup(char *str)
 	char *s;
 	int i, len;
 
-	len = strlen(str);
 	if (str == NULL)
 	{
 		return (NULL);
-	}
+	}	
+	len = strlen(str);
 	s = malloc(sizeof(char) * len + 1);
 	if (s == NULL)
 	{
