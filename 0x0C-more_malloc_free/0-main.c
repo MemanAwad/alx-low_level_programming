040337d27f617f86158506b633101e92a8d691b2
MemanAwad <meman1awad@gmail.come>
2023-09-07 12:54:01 -0700

    calloc

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -1,6 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ *  * simple_print_buffer - prints buffer in hexa
+ *   * @buffer: the address of memory to print
+ *    * @size: the size of the memory to print
+ *     *
+ *      * Return: Nothing.
+ *       */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
 
 /**
  *  * main - check the code
@@ -9,16 +38,13 @@
  *     */
 int main(void)
 {
-	char *concat, *cat, *con;
+	char *a;
 
-	concat = string_nconcat(NULL, "school", 6);
-	printf("%s\n", concat);
-	cat = string_nconcat("best", NULL, 6);
-	printf("%s\n", cat);
-	con = string_nconcat("best", "schoolllllll", 6);
-	printf("%s\n", con);
-	free(concat);
-	free(cat);
-	free(con);
+	a = _calloc(98, sizeof(char));
+	strcpy(a, "Best");
+	strcpy(a + 4, " School! :)\n");
+	a[97] = '!';
+	simple_print_buffer(a, 98);
+	free(a);
 	return (0);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _memset - fills first n bytes with constant byte b
+ * @s: the string
+ * @b: this filling byte
+ * @n: number of bytes to be filled
+ * Return: string s
+ */
+
+void _memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+}
 /**
  * _calloc - allocate memory for an array using calloc
- * @b: number of element
+ * @nmemb: number of element
  * @size: size of byte
  * Return: void pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
 
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr != NULL)
+	{
+		_memset(ptr, 0, nmemb * size);
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
