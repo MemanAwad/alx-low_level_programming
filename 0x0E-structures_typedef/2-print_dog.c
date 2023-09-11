c8be8f6873ce5ce5fe9fc1b8a6b76f8e8678070a
MemanAwad <meman1awad@gmail.come>
2023-09-11 14:42:23 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", s);
 	}
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	if ((*d).owner != NULL)
 	{
 		printf("Owner: %s\n", d->owner);
