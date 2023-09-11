db8503f111164a36ebfff532bb4bedf157e3dd26
MemanAwad <meman1awad@gmail.come>
2023-09-11 12:41:32 -0700

    dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a function and typedef programs directoty
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure about dog
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ *
+ * Description: it contains the dog name, age, and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
