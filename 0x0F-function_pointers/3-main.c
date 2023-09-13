8d2493ec7d973ffb56ad34b38976b3e272ec808f
MemanAwad <meman1awad@gmail.come>
2023-09-13 13:39:11 -0700

    perform calculations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - run functions to perform mathmatical operation
+ * Return: int
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - will add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - return difference between two integer
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - return result of divide two number
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - return remider of division of two integer
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
