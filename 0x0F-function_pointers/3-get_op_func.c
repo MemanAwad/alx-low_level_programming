01b4c53dcf86f70302781bd9e4fea8d4c2de5121
MemanAwad <meman1awad@gmail.come>
2023-09-13 14:21:34 -0700

    perform calculations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,27 @@
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-};
-int i;
+/** 
+ * get_op_func - return the right function
+ * @s: the operator
+ * Return: pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i][0] != NULL)
+	{
+		if (*s == op[i][0])
+		{
+			return (op[i][i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -7,5 +8,26 @@
  */
 int main(void)
 {
+	char *str;
+	int (*ptr_get_op_func(char *s))(int, int);
+
+	scanf("%s", &str);
+
+	if (strlen(str) > 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	ptr_get_op_func = get_op_func();
+	if (ptr_get_op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (0);
 }
