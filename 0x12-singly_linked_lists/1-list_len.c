1101c19d82c68a9e0034a9b2d0341bdef6b1b43a
MemanAwad <meman1awad@gmail.com>
2023-09-21 17:13:59 +0200

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - this function will count the lenght of s linked list
+ * @h: the head of the linked list
+ * Return: number of the node
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+	}
+	return (count);
+}
