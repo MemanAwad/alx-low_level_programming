f69b6d5ebd168b59dbd9f37a3b1e0d553d40bc3d
MemanAwad <meman1awad@gmail.com>
2023-09-21 20:11:37 +0200

    delete linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - this function will delete the element of s linked list
+ * @head: the head of the linked list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
