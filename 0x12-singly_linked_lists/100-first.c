9dad5f4a06a7b3f92528426330eb19fdef194264
MemanAwad <meman1awad@gmail.com>
2023-09-22 00:13:36 +0200

    function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include "lists.h"
 /**
  * start_up - function execued before main to print some text
  * Return: void
  */
-void start_up(void)
+void start_up()
 {
 	printf("You're beat! and yet, you must allow,\n
 			I bore my house upon my back!\n");
