186ae3ab335a0d80c926fbc215d203e9a1956078
MemanAwad <meman1awad@gmail.com>
2023-09-25 21:08:52 +0200

    function to delete linked list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->next);
 		free(temp);
 	}
 	free(head);
