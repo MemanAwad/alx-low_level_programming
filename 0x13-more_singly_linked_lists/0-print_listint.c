93072d6981bbeec399018a6b42ea7844498539f5
MemanAwad <meman1awad@gmail.com>
2023-09-25 19:46:02 +0200

    function to print linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - it will print the linked list ineger value
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t total_node = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		total_node++;
+	}
+	return (total_node);
+}
+
