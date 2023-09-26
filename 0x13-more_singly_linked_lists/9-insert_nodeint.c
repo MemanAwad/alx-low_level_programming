400c5e28d60223abc2e574755b4387ffa55286e8
MemanAwad <meman1awad@gmail.com>
2023-09-26 23:34:22 +0200

    function insert noe at given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		count++;
 	}
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || idx > count)
+	if (new == NULL || idx >= count)
 	{
 		return (NULL);
 	}
