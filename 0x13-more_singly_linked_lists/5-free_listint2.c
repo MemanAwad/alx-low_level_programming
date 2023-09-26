3c68ef07547b3b87b997ce55057431dc52a33c08
MemanAwad <meman1awad@gmail.com>
2023-09-26 19:29:19 +0200

    function delete sll

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function to delete linked list
+ * @head: the head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	free(*head);
+	*head = NULL;
+}
+
