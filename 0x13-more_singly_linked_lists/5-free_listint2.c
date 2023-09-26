dfad26626bef88bbd8d896d8fbbeda14d6fabd28
MemanAwad <meman1awad@gmail.com>
2023-09-26 19:40:38 +0200

    function delete linked list and set the head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
 	free(*head);
-	*head = NULL;
+	head = NULL;
 }
