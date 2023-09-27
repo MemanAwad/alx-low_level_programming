5e00affa84bf4994cea3ffbd4aa74ea4e4a95160
MemanAwad <meman1awad@gmail.com>
2023-09-27 15:41:40 +0200

    free linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * free_listint2 - function to delete linked list
  * @head: the head of the list
@@ -8,6 +7,11 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
+	if (head == NULL)
+	{
+		return;
+	}
 
 	while (*head != NULL)
 	{
