e0260e5a88b3b680340a409bc27eaf688541b064
MemanAwad <meman1awad@gmail.com>
2023-09-27 16:22:59 +0200

    delete node at the given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * delete_nodeint_at_index - delete node from the given index
  * @head: the head of the list
@@ -9,7 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	unsigned int count;
+	unsigned int count = 0;
 	listint_t *temp = *head;
 	listint_t *temp2;
 
