307591f34259d513eb4c4d6d055dcfafe9ab6dc8
MemanAwad <meman1awad@gmail.com>
2023-09-28 19:07:55 +0200

    function to convert integer to binary and print it

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rec - convert integer to binary then print it
+ * @n: integer value
+ * Return: void
+ */
+void rec(unsigned long int n)
+{
+	int num = n;
+	char c;
+
+	if(num == 0)
+		return;
+	c = (num & 1) + '0';
+	num >>= 1;
+	rec(num);
+	_putchar(c);
+}
+
+/**
+ * print_binary - this function convert integer to binary and print it
+ * @n: the integer number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		rec(n);
+	}
+
+}
