c37ceb6b98cace7be109deb7159ebf1f7609d5b1
MemanAwad <meman1awad@gmail.com>
2023-10-03 00:36:11 +0200

    function to copy the content of the file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,66 @@
 #include "main.h"
 #include <stdio.h>
 #define BUFF_SIZE 1024
+
+/**
+ * readfrom - function to read from a given file
+ * @filefrom: file tp be read from
+ * @buffer: the buffer to store the read data
+ * @fd: file descripptor
+ * Return: number of read byte
+ */
+
+static ssize_t readfrom(char *filefrom, char **buffer, int fd)
+{
+	int readd;
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filefrom);
+		exit(98);
+	}
+	if (!(*buffer))
+		*buffer = malloc(sizeof(char) * BUFF_SIZE);
+	if (!(*buffer))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filefrom);
+		exit(98);
+	}
+	readd = read(fd, *buffer, BUFF_SIZE);
+	if (readd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filefrom);
+		exit(98);
+	}
+	return (readd);
+}
+
+/**
+ * write_to_file - write the content of the bufer to the file
+ * @file_to: the file to write to
+ * @fd: file descriptor
+ * @buffer: the content to be written
+ * @len: lenght of byte to be written
+ * Return: void
+ */
+
+static void write_to_file(char *file_to, int fd, char *buffer, int len)
+{
+	if (fd < 0 || !buffer)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	if (write(fd, buffer, len) < 0)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+}
+
 /**
  * main - copy the content of one file to another
  * @ac: number of argument
@@ -10,10 +70,12 @@
 
 int main(int ac, char **av)
 {
-	int fd1, fd2, wr, cl, cl2, re;
+	int fd1, fd2, cl, cl2;
 	char *src, *dest;
 	char *buffer;
+	int re = 1;
 
+	buffer = NULL;
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -23,31 +85,15 @@ int main(int ac, char **av)
 	dest = av[2];
 	fd1 = open(src, O_RDONLY);
 	fd2 = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	buffer = malloc(sizeof(char) * BUFF_SIZE);
-	if (fd2 < 0 || buffer == NULL)
+
+	while (re > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		free(buffer);
-		exit(98);
+		re = readfrom(src, &buffer, fd1);
+		if (!re)
+			break;
+		write_to_file(dest, fd2, buffer, re);
 	}
-	re = read(fd1, buffer, BUFF_SIZE);
-	if (re < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		free(buffer);
-		exit(98);
-	}
-	if (fd1 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-		exit(99);
-	}
-	wr = write(fd2, buffer, re);
-	if (wr < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-		exit(99);
-	}
+	free(buffer);
 	cl = close(fd1);
 	cl2 = close(fd2);
 	if (cl < 0)
