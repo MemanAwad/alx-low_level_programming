ecbceb28a77d88eb9cfe7f1cdc2ab6d95e6ff222
MemanAwad <meman1awad@gmail.com>
2023-10-02 20:00:28 +0200

    function to create a file with given content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,12 +34,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd < 0)
 		return (-1);
 	len = _strlen(text_content);
 	wr = write(fd, text_content, len);
-	if (wr <= 0)
+	if (wr < 0)
 		return (-1);
 
 	close(fd);
