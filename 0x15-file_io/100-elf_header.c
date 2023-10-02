f0195186b5186211db6e9aafb2f0afc0d9bb682b
MemanAwad <meman1awad@gmail.com>
2023-10-03 00:46:34 +0200

    elf header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - display informatio contained in ELF header
+ * @ac: the argument
+ * @av: the array of argument
+ * Return: Always 0
+ */
+int main(int ac, char **av)
+{
+	return(0);
+}
