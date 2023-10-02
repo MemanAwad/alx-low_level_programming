26485508f2a6dda84f8ac1bb87e25b7287e1ac8b
MemanAwad <meman1awad@gmail.com>
2023-10-02 19:47:51 +0200

    function to create a file with given content

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _strlen - function count the length of the string
+ * @str: the string
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+	int i = 0;
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\0')
+	{
+		i++;
+		count++;
+	}
+	return (count);
+}
+/**
+ * create_file - function to create a file with given contents
+ * @filename: the name of the file
+ * @text_content: null terminated string to be written in the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *test_content)
+{
+	int fd;
+	int wr;
+	int len;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd < 0)
+		return (-1);
+	len = _strlen(test_content);
+	wr = write(fd, test_content, len);
+	if (wr <= 0 )
+		return (-1);
+
+	close(fd);
+	return (1);
+}
