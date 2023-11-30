25853bdc2d72c1468482efb9f782da3d563d19ab
MemanAwad <meman1awad@gmail.com>
2023-11-30 18:23:15 +0200

    header file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,6 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i = 0;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
@@ -16,10 +15,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
-	for (i = 0; i < size; i++)
-	{
-		table->array[i] = NULL;
-	}
+	table->array = malloc(sizeof(hash_node_t*));
 	return (table);
 }
