5d9d7af2e756c75a68866bf1df364df26339d435
MemanAwad <meman1awad@gmail.com>
2023-11-30 16:19:44 +0200

    hash table directory

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create hash table
+ * @size: the size of the hash table
+ *
+ * Return: the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size);
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This directory contains programs that creates hash tables and make some process at it
