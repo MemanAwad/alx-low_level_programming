b84d3466604b08d6d7bb0148585ffc6bbae28975
MemanAwad <meman1awad@gmail.com>
2023-11-30 18:13:36 +0200

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,4 +6,20 @@
  *
  * Return: the newly created hash table
  */
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	long unsigned int i = 0;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return table;
+}
