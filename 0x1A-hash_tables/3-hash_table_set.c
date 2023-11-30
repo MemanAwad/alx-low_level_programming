031d107a40e843df0fb117a80b01d5033472d55d
MemanAwad <meman1awad@gmail.com>
2023-11-30 22:22:33 +0200

    add a new set to the table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,23 +13,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	hash_node_t *new_set = NULL;
+	hash_node_t *current = NULL;
 
 	if (key == NULL)
 		return (0);
 
+	new_set = malloc(sizeof(hash_node_t));
 	new_set->key = strdup(key);
 	new_set->value = strdup(value);
 	new_set->next = NULL;
-
+	
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	current = ht->array[index];
+	if (current == NULL)
 	{
 	       ht->array[index] = new_set;
 	       return (1);
 	}
 	else
 	{
-		new_set->next = ht->array[index];
+		while(current->next != NULL)
+		{
+			current = current->next;
+		}
+		new_set->next = current;
 		ht->array[index] = new_set;
 	}
 	return (1);
