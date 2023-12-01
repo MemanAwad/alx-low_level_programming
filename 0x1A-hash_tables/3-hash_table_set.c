f0dc4844191f23c1d87945dd36a44ae1c3bd791c
MemanAwad <meman1awad@gmail.com>
2023-12-01 17:01:16 +0200

    adding set to the table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		if (strcmp(current->key, key) == 0)
+		if (strcmp(current->key, key) == 0 && current->next == NULL)
 		{
 			strcpy(ht->array[index]->value, value);
 			return (1);
