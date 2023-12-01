a58ac04c63e6a9bce73027cb3fdb365b80aa8cf9
MemanAwad <meman1awad@gmail.com>
2023-12-01 20:36:01 +0200

    function that adds new set to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,43 +11,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	hash_node_t *new_set = NULL;
-	hash_node_t *current = NULL;
+	unsigned long int index, i;
+	hash_node_t *new = NULL;
+	char *value_copy;
 
 	if (!ht || strcmp(key, "") == 0 || !key || !value)
 		return (0);
-	new_set = malloc(sizeof(hash_node_t));
-	if (!new_set)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	new_set->key = malloc(sizeof(char) * strlen(key) + 1);
-	new_set->value = malloc(sizeof(char) * strlen(value) + 1);
-	strcpy(new_set->key, key);
-	strcpy(new_set->value, value);
-	index = key_index((unsigned char *)key, ht->size);
-	current = ht->array[index];
-	if (current == NULL)
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		new_set->next = NULL;
-		ht->array[index] = new_set;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
 	}
-	while (current)
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		if (strcmp(current->key, key) == 0)
-			break;
-		current = current->next;
+		free(value_copy);
+		return (0);
 	}
-	if (current)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(current->value);
-		current->value = malloc(sizeof(char) * strlen(value) + 1);
-		if (!current->value)
-			return (0);
-		strcpy(current->value, value);
-		return (1);
+		free(new);
+		return (0);
 	}
-	new_set->next = ht->array[index];
-	ht->array[index] = new_set;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
