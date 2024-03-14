2a8afc6f6318a86312b0412eb711552b2f940c9d
MemanAwad <meman1awad@gmail.com>
2024-03-14 04:53:16 +0200

    binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+/**
+ * linear_search - function that implement the binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: index of the value of -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, high, low;
+	
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+
+	high = size - 1;
+	low = 0;
+
+	while (high >= low)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+
+		i = low + (high - low) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			high = i - 1;
+		else
+			low = i + 1;
+	}
+	return (-1);
+}
